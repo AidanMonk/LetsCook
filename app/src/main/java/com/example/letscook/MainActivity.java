9ea7cbf7b031aeadbe8fde60bd7acae7523cbf71
YunxianXu <xuyunxian.12@gmail.com>
2024-10-31 17:35:45 -0400

    test commit

--- app/src/main/java/com/example/letscook/MainActivity.java
+++ app/src/main/java/com/example/letscook/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
